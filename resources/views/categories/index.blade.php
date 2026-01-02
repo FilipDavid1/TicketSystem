7367b1240ddf4bca4b9c1102bf1119feda8a0b6e
FilipDavid1 <filipenkodavid@gmail.com>
2026-01-02 19:20:21 +0100

    fix(category): be able to delete/eddit category only if - user is superadmin/creator

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -71,6 +71,7 @@
                 </div>
 
                 <div class="d-flex gap-2 mt-auto">
+                    @if(Auth::user()->role === 'superadmin' || $category->created_by === Auth::id())
                     <button type="button"
                         class="light-btn flex-fill d-flex align-items-center justify-content-center gap-2"
                         @click="openEditModal({{ $category->id }}, '{{ addslashes($category->name) }}', '{{ addslashes($category->description ?? '') }}', {{ json_encode($category->admins->pluck('id')) }})">
@@ -87,6 +88,11 @@
                             <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
                         </svg>
                     </button>
+                    @else
+                    <div class="text-muted small flex-fill text-center py-2">
+                        <em>Nemôžete upravovať túto kategóriu</em>
+                    </div>
+                    @endif
                 </div>
             </div>
         </div>
